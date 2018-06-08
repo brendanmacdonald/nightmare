b545abf9219e5bdf6b76bb8d6987ba6bc7faf94f
Brendan MacDonald <bmacdonald@scottlogic.co.uk>
2018-06-08 15:49:27 +0100

    Tidy mocha test

--- test/mocha.js
+++ test/mocha.js
@@ -2,7 +2,7 @@ const Nightmare = require('nightmare')
 const expect = require('chai').expect
 const obj = require('../utils/page_objects')
 
-describe('Load a Page', function () {
+describe('BBC Home Page', function () {
     this.timeout('5s')
 
     let nightmare = null
@@ -10,17 +10,18 @@ describe('Load a Page', function () {
         nightmare = new Nightmare()
     })
 
-    describe('/ (Home Page)', () => {
-        it('should load without error', done => {
-            nightmare
-                .goto(obj.baseURL)
-                .title()
-                .end()
-                .then((title) => {
-                    expect(title).to.equal('BBC - Home')
-                    done();
-                })
-                .catch(done)
-        })
+    afterEach(() => {
+        nightmare.end();
+    })
+
+    it('should load without error', done => {
+        nightmare
+            .goto(obj.baseURL)
+            .title()
+            .end()
+            .then((title) => {
+                expect(title).to.equal('BBC - Home');
+                done();
+            })
     })
 })
