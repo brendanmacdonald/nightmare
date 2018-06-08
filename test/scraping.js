2f5c89e232661c6351020b1519f583685bc3e47d
Brendan MacDonald <bmacdonald@scottlogic.co.uk>
2018-06-08 11:56:25 +0100

    Tidy scraping test

--- test/scraping.js
+++ test/scraping.js
@@ -1,28 +1,22 @@
 const Nightmare = require('nightmare')
-const HACKER_NEWS_URL = 'https://news.ycombinator.com/'
-const storyLinkSelector = '.storylink'
-
-Nightmare()
-    .goto('http://www.bbc.co.uk/news')
-    .evaluate(() => Array.from(document.querySelectorAll('.nw-c-most-read__items .gs-o-media__body')).map(el => el.innerText))
-    .end()
-    .then(result => {
-        for (var i = 0; i < result.length; i++) {
-            console.log(`\n${result[i]}`)
-        }
-    })
+const baseURL = 'http://www.bbc.co.uk/news'
+const mostReadSelector = '.nw-c-most-read__items .gs-o-media__body';
 
 Nightmare({
         show: false,
         openDevTools: false,
         waitTimeout: 2000
     })
-    .goto(HACKER_NEWS_URL)
+    .goto(baseURL)
     .evaluate(storySelector => {
         var storyElements = [].slice.call(document.querySelectorAll(storySelector))
         var storyTitles = storyElements.map(storyElement => storyElement.innerText)
         return storyTitles
-    }, storyLinkSelector)
+    }, mostReadSelector)
     .end()
-    .then(result => console.log(result))
+    .then(titles => {
+        for (var title in titles) {
+            console.log(`${titles[title]}`)
+        }
+    })
     .catch(error => console.error(error))
